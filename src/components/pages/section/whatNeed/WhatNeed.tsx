888e08fe957130b45e50f014f112cf146bf4196c
NurlanPalvanAke <artyom.valeriyevich@gmail.com>
2023-06-08 21:57:02 +0600

    add new section whatNedd

--- src/components/pages/section/whatNeed/WhatNeed.tsx
+++ src/components/pages/section/whatNeed/WhatNeed.tsx
@@ -7,6 +7,7 @@ import cardImgFive from '../../../../assets/whatNeed/five.png'
 import cardImgSix from '../../../../assets/whatNeed/six.png'
 import cardImgSeven from '../../../../assets/whatNeed/seven.png'
 import cardImgEight from '../../../../assets/whatNeed/eight.png'
+import cardImgHome from '../../../../assets/whatNeed/home.png'
 
 
 const WhatNeed = () => {
@@ -15,6 +16,11 @@ const WhatNeed = () => {
             <div className="container">
                 <h3 className="whatNeed__title">Что нужно делать, чтобы получить визу в США самостоятельно</h3>
                 <div className="whatNeed__row">
+
+                    <div className="whatNeed__card">
+                        <img src={cardImgHome} alt="" className="whatNeed__card-image"/>
+                    </div>
+
                     <div className="whatNeed__card">
                         <img src={cardImgOne} alt="" className="whatNeed__card-img"/>
                         <h3 className="whatNeed__card-title">
@@ -28,8 +34,8 @@ const WhatNeed = () => {
                     <div className="whatNeed__card">
                         <img src={cardImgTwo} alt="" className="whatNeed__card-img"/>
                         <h3 className="whatNeed__card-title">
-
-                            <span className="whatNeed__card-title-bold"></span>
+                            Оплатить <br/>
+                            <span className="whatNeed__card-title-bold">консульский взнос</span>
 
                         </h3>
                     </div>
@@ -37,8 +43,8 @@ const WhatNeed = () => {
                     <div className="whatNeed__card">
                         <img src={cardImgThree} alt="" className="whatNeed__card-img"/>
                         <h3 className="whatNeed__card-title">
-
-                            <span className="whatNeed__card-title-bold"></span>
+                            Зарегистрироваться на сайте посольства США. Прикрепить
+                            <span className="whatNeed__card-title-bold">заполненную анкету и оплаченную квитанцию</span>
 
                         </h3>
                     </div>
@@ -46,8 +52,8 @@ const WhatNeed = () => {
                     <div className="whatNeed__card">
                         <img src={cardImgFour} alt="" className="whatNeed__card-img"/>
                         <h3 className="whatNeed__card-title">
-
-                            <span className="whatNeed__card-title-bold"></span>
+                            Сделать фотографию
+                            <span className="whatNeed__card-title-bold">верного формата</span>
 
                         </h3>
                     </div>
@@ -55,8 +61,8 @@ const WhatNeed = () => {
                     <div className="whatNeed__card">
                         <img src={cardImgFive} alt="" className="whatNeed__card-img"/>
                         <h3 className="whatNeed__card-title">
-
-                            <span className="whatNeed__card-title-bold"></span>
+                            Собрать
+                            <span className="whatNeed__card-title-bold">большой перечень  <br/> документов</span>
 
                         </h3>
                     </div>
@@ -64,8 +70,8 @@ const WhatNeed = () => {
                     <div className="whatNeed__card">
                         <img src={cardImgSix} alt="" className="whatNeed__card-img"/>
                         <h3 className="whatNeed__card-title">
-
-                            <span className="whatNeed__card-title-bold"></span>
+                            Отслеживать днями и ночами, когда на сайте консульства появится
+                            <span className="whatNeed__card-title-bold">окно записи на собеседование</span>
 
                         </h3>
                     </div>
@@ -73,8 +79,8 @@ const WhatNeed = () => {
                     <div className="whatNeed__card">
                         <img src={cardImgSeven} alt="" className="whatNeed__card-img"/>
                         <h3 className="whatNeed__card-title">
-
-                            <span className="whatNeed__card-title-bold"></span>
+                            Подготовиться к непростому
+                            <span className="whatNeed__card-title-bold">собеседованию с консулом</span>
 
                         </h3>
                     </div>
@@ -82,8 +88,8 @@ const WhatNeed = () => {
                     <div className="whatNeed__card">
                         <img src={cardImgEight} alt="" className="whatNeed__card-img"/>
                         <h3 className="whatNeed__card-title">
-
-                            <span className="whatNeed__card-title-bold"></span>
+                            Мониторить сайт и отслеживать
+                            <span className="whatNeed__card-title-bold">результаты готовности визы</span>
 
                         </h3>
                     </div>
